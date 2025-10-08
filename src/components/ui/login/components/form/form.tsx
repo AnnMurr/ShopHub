f0edc4f9c5f7c6d81986bc167767514e5f6afdce
AnnMurr <113177152+AnnMurr@users.noreply.github.com>
2025-10-08 17:20:03 +0300

    add validation to form

--- src/components/ui/login/components/form/form.tsx
+++ src/components/ui/login/components/form/form.tsx
@@ -6,11 +6,42 @@ import Typography from "@mui/material/Typography";
 import { useState } from "react";
 import { IconButton, InputAdornment } from "@mui/material";
 import { Visibility, VisibilityOff } from "@mui/icons-material";
+import { passwordRegex, emailRegex } from "../../../../../consts/consts";
+
+interface ErrorsType {
+    email: string,
+    password: string
+}
 
 export const Form = () => {
-    const [showPassword, setShowPassword] = useState(false);
+    const [showPassword, setShowPassword] = useState<boolean>(false);
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const [errors, setErrors] = useState<ErrorsType>({ email: "", password: "" });
 
     const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+    const validateForm = () => {
+        let valid: boolean = true
+        const newErrors: ErrorsType = { email: "", password: "" };
+
+        if(!emailRegex.test(email)) {
+            newErrors.email = "invalid email";
+            valid = false
+        }
+
+        if(!passwordRegex.test(password)) {
+            newErrors.password = "The password must be at least 8 characters long, with at least 1 lowercase letter, 1 uppercase letter, and 1 symbol";
+            valid = false
+        }
+
+        setErrors(newErrors);
+        return valid;
+    }
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+    }
 
     return (
         <Paper
@@ -29,20 +60,28 @@ export const Form = () => {
             <Typography variant="h5" align="center" gutterBottom>
                 LOGIN
             </Typography>
-            <Box component="form" >
+            <Box component="form" onSubmit={handleSubmit} >
                 <TextField
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
                     name="email"
                     label="Email"
-                    type="email"
+                    type="text"
                     fullWidth
                     sx={{ mb: 2, backgroundColor: "#fff" }}
+                    error={!!errors.email}
+                    helperText={errors.email}
                 />
                 <TextField
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
                     name="password"
                     label="Password"
                     type={showPassword ? "text" : "password"}
                     fullWidth
                     sx={{ mb: 2 }}
+                    error={!!errors.password}
+                    helperText={errors.password}
                     slotProps={{
                         input: {
                             endAdornment: (
---
+++ src/consts/consts.ts
@@ -0,0 +1,2 @@
+export const emailRegex = /^\S+@\S+\.\S+$/;
+export const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).{8,}$/;
