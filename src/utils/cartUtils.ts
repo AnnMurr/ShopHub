4f4cebcdc24a4f190dea889329a277ed81b9caf6
AnnMurr <113177152+AnnMurr@users.noreply.github.com>
2025-10-16 17:30:00 +0300

    fix countTotalPrice function

--- src/utils/cartUtils.ts
+++ src/utils/cartUtils.ts
@@ -5,7 +5,7 @@ export const countQuantity = (items: Array<CartItem>): number => {
 }
 
 export const countTotalPrice = (items: Array<CartItem>): number => {
-    return items.reduce((acc, item) => acc += item.price, 0);
+    return items.reduce((acc, item) => acc += item.price * item.quantity, 0);
 }
 
 export const setCartDataToLocalStorage = (items: Array<CartItem>) => {
