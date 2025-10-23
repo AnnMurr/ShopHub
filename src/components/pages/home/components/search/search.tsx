ca3cecee71bffa5b1e5a79955308de2f69f1b321
AnnMurr <113177152+AnnMurr@users.noreply.github.com>
2025-10-23 11:25:35 +0300

    change search logic

--- src/components/pages/home/components/search/search.tsx
+++ src/components/pages/home/components/search/search.tsx
@@ -1,17 +1,23 @@
-import SearchIcon from '@mui/icons-material/Search';
-import IconButton from '@mui/material/IconButton';
 import InputBase from '@mui/material/InputBase';
 import Paper from '@mui/material/Paper';
-import { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 
 export const Search = () => {
   const [searchValue, setSearchValue] = useState<string>('');
   const [searchParams, setSearchParams] = useSearchParams();
 
-  const handleSearch = () => {
-    setSearchParams(searchValue.trim() ? { search: searchValue.trim() } : {});
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(e.target.value);
   };
+
+  useEffect(() => {
+    const handleSearch = setTimeout(() => {
+      setSearchParams(searchValue.trim() ? { search: searchValue.trim() } : {});
+    }, 300);
+
+    return () => clearTimeout(handleSearch);
+  }, [searchValue]);
 
   return (
     <Paper
@@ -23,27 +29,18 @@ export const Search = () => {
         alignItems: 'center',
         maxWidth: '80%',
         m: '90px auto 0 auto',
-        '@media screen and (max-width: 760px)': {
+
+        '@media (max-width: 760px)': {
           maxWidth: '100%',
         },
       }}
     >
       <InputBase
-        onKeyDown={(e) => e.key === 'Enter' && handleSearch()}
-        value={searchValue}
-        onChange={(e) => setSearchValue(e.target.value)}
+        onChange={handleOnChange}
         sx={{ ml: 1, flex: 1 }}
         placeholder="Search"
         inputProps={{ 'aria-label': 'search' }}
       />
-      <IconButton
-        onClick={handleSearch}
-        type="button"
-        sx={{ p: '10px' }}
-        aria-label="search"
-      >
-        <SearchIcon />
-      </IconButton>
     </Paper>
   );
 };
